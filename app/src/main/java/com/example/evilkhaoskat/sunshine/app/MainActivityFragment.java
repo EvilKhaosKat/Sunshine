45554325ea3f5f2aff61c6d83a64d3164c0bce82
EvilKhaosKat <evilkhaoskat@gmail.com>
2015-08-02 13:35:54 +0400

    list item with fake data

--- app/src/main/java/com/example/evilkhaoskat/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/evilkhaoskat/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,6 +23,26 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> weekForecast = new ArrayList<>();
+        weekForecast.add("Today - Sunday - 23/15");
+        weekForecast.add("Tomorrow - Foggy - 20/10");
+        weekForecast.add("Weds - Cloudy - 22/12");
+        weekForecast.add("Thurs - Rainy  - 22/12");
+        weekForecast.add("Fri - Sunny - 25/16");
+        weekForecast.add("Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18");
+        weekForecast.add("Sun 6/29 - Sunny - 20/7");
+
+        ArrayAdapter<String> forecastListAdapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list_item_forecast);
+        listView.setAdapter(forecastListAdapter);
+
+        return rootView;
     }
 }
